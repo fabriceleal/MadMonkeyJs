7bf090816c8f47def01678a3fb561130acfab4b5
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-09-06 22:18:21 +0100

    some cleanup in the example

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-A lib for generating random javascript programs
-***
+# MadMonkeyJs
 
-TODO description and example
+A node.js module for generating random javascript programs
+
+## Installing
+
+```shell
+npm install madmonkey
+```
+
+## Using
+
+Check example/main.js for a working example!
+
+Usage: 
+
+```javascript
+	var madmonkey = require("madmonkey");
+
+	// Create a generator with the "signature" of the program
+	// The signature is the type of the parameters (or lack of them...) and the type of the return value (or lack of it ...)
+
+	// Our program will take no parameters and return a number
+	var g = new Generator('_->number');
+
+	// Add "forms" to the generator
+	// A form is a valid element inside the program
+	// A form can be:
+	//		* A constant (ex: '(80)')
+	//		* A built-in function (ex: '(Math.cos)')
+	//		* A custom function (ex: '(function(x){ return x * 2;})')
+	// All forms should be provided with their signature
+	//
+
+```
--- example/main.js
+++ example/main.js
@@ -6,37 +6,31 @@ with(require("madmonkey")){
 	var g = new Generator('_->number');
 
 	// Add forms to the generator
-	g.addForm('(Math.sin)', 												'(number)->number' ).
-			addForm('(Math.cos)', 											'(number)->number' ).
-			addForm('(Math.sqrt)', 											'(number)->number' ).
-			addForm('(Math.tan)', 											'(number)->number' ).
-			addForm('(180)', 													'number');
+	g.addForm('(Math.sin)', '(number)->number' ).
+			addForm('(Math.cos)', '(number)->number' ).
+			addForm('(Math.sqrt)', '(number)->number' ).
+			addForm('(Math.tan)', '(number)->number' ).
+			addForm('(180)', 'number');
 	// ---
 
 	var g2 = new Generator('_->number');
 
-	g2.addForm('(function(x){ return Math.random() * x})', 	'(number)->number' ).
-			addForm('(function(x){ return Math.random() * x * 2})', 	'(number)->number' ).
-			addForm('(function(x){ return Math.random() * x / 2})', 	'(number)->number' ).
-			//addForm('(0)', 													'number').
-			//addForm('(180)', 													'number').
-			//addForm('(270)', 													'number').
-			addForm('(360)', 													'number');
+	g2.addForm('(function(x){ return Math.random() * x})', '(number)->number' ).
+			addForm('(function(x){ return Math.random() * x * 2})', '(number)->number' ).
+			addForm('(function(x){ return Math.random() * x / 2})', '(number)->number' ).
+			addForm('(360)', 'number');
 	// ---
 
-	var t = g.gen(10);						// Generate a tree with a maximum depth of 10
+	// Generate a tree with a maximum depth of 10
+	var t = g.gen(10);						
 	var t2 = g2.gen(10);
 
-	console.log( 't = ' + t.compile() ); 			// Compile to javascript code
+	// Compile to javascript code
+	console.log( 't = ' + t.compile() ); 			
 	console.log( 't2 = ' + t2.compile() )
 
 	var e = t2.extract();
 	console.log( 'extracted = ' + e.compile() );
 	console.log( 'hybrid = ' + t.inject(e).compile() );
-
-//	console.log(  t.eval() );				// Interpret the internal tree representation
-//	console.log(  eval(t.compile()) ); 	// Eval the compilation of the javascript code
-//	console.log( 'size = ' + t.codeSize() );
-//	console.log( t.extract().compile() );
 
 }
